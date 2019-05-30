5fac0b176c4cdfeceba355734db473fb2956088d
mr-fool <mr-fool@users.noreply.github.com>
2019-05-30 15:55:17 -0600

    Update core.js

--- core.js
+++ core.js
@@ -1,5 +1,16 @@
+//Importing all the dependencies
+
 const request = require('request-promise');
 const fs = require('fs');
-
-const Temporal = require('..');
 const Utils = require('./temproal-js/test/utils');
+const Temporal = require('./temporal-js');
+
+const temporal = new Temporal();
+
+temporal.register("user", "email", "password")
+.then(() => {
+    console.log('User is registered!');
+})
+.catch((err) => {
+    console.log("error!");
+})
